a8e83c78984d49a930275b5ec1d3f30942ed6914
Pu Anlai <inspectormustache@users.noreply.github.com>
2017-10-14 01:11:53 +0200

    Populate menu list

--- shan/tui.py
+++ shan/tui.py
@@ -1,25 +1,74 @@
 import curses
+from collections import namedtuple
+from time import sleep
 
-def main(stdscr):
-    """Main curses function to be run from wrapper."""
-    stdscr.clear()
-    curses.curs_set(0)
+# $value represents what is returned when hitting enter, $str_ represents the
+# menu entry as it appears to the user
+MenuEntry = namedtuple('MenuEntry', ['value', 'str_'])
 
-    base_win = curses.newwin(curses.LINES - 2, curses.COLS - 2, 1, 1)
-    base_win.border()
-    base_win_height, base_win_width = base_win.getmaxyx()
-    sub_win = base_win.derwin(base_win_height - 3, base_win_width - 5, 2, 3)
 
-    for line in range(15):
-        sub_win.addstr(line + 1, 2, 'miau')
+class Menu():
+    """Menu class with navigation methods. Each menu entry must be an
+    MenuEntry. Constructing requires a list of MenuEntry objects and a stdscr
+    object from curses.wrapper."""
 
-    stdscr.noutrefresh()
-    base_win.noutrefresh()
-    curses.doupdate()
-    while True:
-        key = stdscr.getkey()
+    def __init__(self, stdscr, entry_list):
+        self.stdscr = stdscr
+        self.entry_dict = self._get_entry_dict(entry_list)
+        self.current_pos = 0
+        # the following method assigns:
+        # self.base_win
+        # self.sub_win
+        self._init_windows(self.stdscr)
 
-        if key == 'q':
-            break
+    def _init_windows(self, stdscr):
+        """Draw initial interface."""
+        stdscr.clear()
+        curses.curs_set(0)
 
-curses.wrapper(main)
+        base_win = curses.newwin(curses.LINES - 2,
+                                 curses.COLS - 2,
+                                 1, 1)
+        base_win.box()
+
+        base_win_height, base_win_width = base_win.getmaxyx()
+        sub_win = base_win.derwin(base_win_height - 2,
+                                  base_win_width - 2,
+                                  1, 1)
+
+        # make windows available to all methods
+        self.base_win = base_win
+        self.sub_win = sub_win
+
+        # create the menulist
+        for pos, entry in self.entry_dict.items():
+            sub_win.addstr(pos, 0, entry.str_)
+
+        self.mark_current_position()
+        self.update_screen()
+
+        sleep(3)  # for debugging only
+
+    def _get_entry_dict(self, entry_list):
+        positions = len(entry_list)
+        entry_dict = {pos: entry_list[pos] for pos in range(positions)}
+        return entry_dict
+
+    def mark_current_position(self):
+        """Mark line at position $pos."""
+        y_pos, x_pos = self.sub_win.getyx()
+        self.sub_win.move(self.current_pos, 0)
+        self.sub_win.chgat(curses.A_REVERSE)
+        # reset cursor position
+        self.sub_win.move(y_pos, x_pos)
+
+    def update_screen(self):
+        """Refresh all windows and redraw screen."""
+        self.stdscr.noutrefresh()
+        self.base_win.noutrefresh()
+        self.sub_win.noutrefresh()
+        curses.doupdate()
+
+
+entries = [MenuEntry(value='bla', str_=x) for x in ['miauli', 'schlauli']]
+curses.wrapper(Menu, entries)
--- tests/test_shan.py
+++ tests/test_shan.py
@@ -1,7 +1,13 @@
-from shan import volumes
+import curses
+from shan import volumes, tui
 
 
 def test_volumes():
     str_volume_list = volumes.get_str_volume_list()
     for str_ in str_volume_list:
         assert isinstance(str_, str)
+
+
+def test_tui():
+    """Test TU interface."""
+    pass
