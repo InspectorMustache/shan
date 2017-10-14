3b0f316ac3c52a1aa8053e12841e5520e0d4d630
Pu Anlai <inspectormustache@users.noreply.github.com>
2017-10-14 23:19:23 +0200

    Implement page indexing

--- shan/tui.py
+++ shan/tui.py
@@ -2,9 +2,16 @@ import curses
 from collections import namedtuple
 from time import sleep
 
+import logging
+logging.basicConfig(filename='debug.log', level=logging.DEBUG)
+
 # $value represents what is returned when hitting enter, $str_ represents the
 # menu entry as it appears to the user
 MenuEntry = namedtuple('MenuEntry', ['value', 'str_'])
+
+# $first is the number of the first entry, $last the number of the final entry
+# + 1 (for use with range())
+MenuPage = namedtuple('MenuPage', ['first', 'last'])
 
 
 class Menu():
@@ -16,10 +23,12 @@ class Menu():
         self.stdscr = stdscr
         self.entry_dict = self._get_entry_dict(entry_list)
         self.current_pos = 0
+        self.current_page = 0
         # the following method assigns:
         # self.base_win
         # self.sub_win
         self._init_windows(self.stdscr)
+        self.pages_dict = self._get_pages_dict()
 
     def _init_windows(self, stdscr):
         """Draw initial interface."""
@@ -49,10 +58,37 @@ class Menu():
 
         sleep(3)  # for debugging only
 
+    # def draw_page(self, page_num):
+    #     """Draw page $page_num."""
+    #     entry_dict_slice = {}
+    #     for entry
+
     def _get_entry_dict(self, entry_list):
         positions = len(entry_list)
         entry_dict = {pos: entry_list[pos] for pos in range(positions)}
         return entry_dict
+
+    def _get_pages_dict(self):
+        """Return a dictionary of pages and their containing entries by
+        position reference."""
+        entry_count = len(self.entry_dict)
+        page_size, _ = self.sub_win.getmaxyx()
+        # page_breaks = those positions at which a page ends and a new one
+        # begins
+        page_breaks = list(range(entry_count))[::page_size]
+
+        pages_dict = {}
+        for iter_, page in enumerate(page_breaks):
+            first_page = page_breaks[iter_]
+            try:
+                last_page = page_breaks[iter_ + 1]
+            except IndexError:
+                last_page = entry_count
+
+            pages_dict[iter_] = MenuPage(first=first_page, last=last_page)
+
+        logging.debug(pages_dict)
+        return pages_dict
 
     def mark_current_position(self):
         """Mark line at position $pos."""
@@ -70,5 +106,5 @@ class Menu():
         curses.doupdate()
 
 
-entries = [MenuEntry(value='bla', str_=x) for x in ['miauli', 'schlauli']]
+entries = [MenuEntry(value='bla', str_='miau'), MenuEntry(value='bla', str_='wuff')]
 curses.wrapper(Menu, entries)
